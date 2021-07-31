48095fc176cb8303c2a161e3ff3a2f2f22254f7f
Parker Gates <parkergates14@gmail.com>
2021-07-31 08:54:25 -0600

    Heroes Listing and List Selection Display Added

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../../interface/hero'
+import { Hero } from '../../interface/hero';
+import { HEROES } from '../../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,9 @@ import { Hero } from '../../interface/hero'
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -16,6 +20,10 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
